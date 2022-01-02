1b4cff850f99d3edb640998c478f55de7d03a10d
Heppers11 <84316957+Heppers11@users.noreply.github.com>
2022-01-02 10:32:32 +1000

    1

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 var params = window.location.href.substring(window.location.href.indexOf('?') + 1).split('&');
+console.log(window.location.href)
 onkeydown = e => e.key == " " ? clicksCountSpan.innerText+=params:0
